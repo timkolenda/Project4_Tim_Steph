551ed94e52790dabac42873e47a82d17f111c8d2
Tim <tkolenda@gmail.com>
2018-11-21 19:36:08 -0500

    lots done

--- script.js
+++ script.js
@@ -1,65 +1,127 @@
 
-//Example API Request
-//https://api.themoviedb.org/3/movie/550?api_key=f0ba00aa70aa95e488fb89869bf99a39
 
+// APP OBJECT
 const movieApp = {}
 
-movieApp.baseURL = "https://api.themoviedb.org/3/";
-movieApp.accessKey = "f0ba00aa70aa95e488fb89869bf99a39";
-movieApp.readAccessToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMGJhMDBhYTcwYWE5NWU0ODhmYjg5ODY5YmY5OWEzOSIsInN1YiI6IjViZjQ3YjM2MGUwYTI2NDMyMDAwYWFiMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PszxkV4DdxWG7lJcIh0PcGSnmleRcQy8m7s3fIydQ8M";
-movieApp.discoverMovie = 'discover/movie';
-movieApp.searchForActor = 'search/person';
-movieApp.find = 'find/movie';
-movieApp.userActorSelection = "Tom Hanks";
 movieApp.userSelectionObject = {};
 
+//VARIABLES
+movieApp.baseURL = "https://api.themoviedb.org/3/";
+movieApp.accessKey = "f0ba00aa70aa95e488fb89869bf99a39";
 
 
+//SEARCH TYPES
+movieApp.discoverMovie = 'discover/movie';
+movieApp.searchForActor = `search/person`;
+movieApp.searchConfig = 'configuration';
+movieApp.searchMovieTrailor = `movie/${movieApp.userSelectionObject.movieID}/videos`;
+
+
+// USER SELECTION
+movieApp.userActorSelection = "Brad Pitt";
+
+//Data Objects
+movieApp.APIOnly = {
+    api_key: movieApp.accessKey,
+}
+
+movieApp.getPersonInfoDataObject = {
+    api_key: movieApp.accessKey,
+    query: movieApp.userActorSelection,
+};
+movieApp.getMovieInfoDataObject = {
+    api_key: movieApp.accessKey,
+    sort_by: 'popularity.asc',
+    with_people: 287
+    // with_people: movieApp.userSelectionObject.userActorSelectionID
+};
+
+
+
+//APP INIT
+movieApp.init = function () {
+    movieApp.getPersonInfo()
+    .then(movieApp.getMovieInfo())
+    // .then(movieApp.getConfiguration())
+
+    
+
+};
 
 $(function () {
     movieApp.init();
-    //NEED TO ASK WHY THE BELOW DOES NOT WORK
-    // $.when(movieApp.getData)
-    //     .then((res) => {
-    //         movieApp.getUserActorSelectionID(res);
-    //     })
 });
 
-movieApp.init = function () {
-    movieApp.getData(movieApp.searchForActor, movieApp.userActorSelection);
-};
 
-movieApp.getData = function(searchType, actorName, actorID){
+movieApp.getData = function(searchType, data){
     return $.ajax({
         url: `${movieApp.baseURL}${searchType}`,
         dataType: 'jsonp',
-        method: 'GET',
-        data: {
-            api_key: 'f0ba00aa70aa95e488fb89869bf99a39',
-            query: actorName,
-            with_people: actorID,
-            vote_average: null,
-            page: null
-            //^^^^^^page and vote_average will be needed once we are pulling movie list
-        }
+        method: 'GET', 
+        data: data
+    }).then((res) => {
+        // console.log(movieApp.userSelectionObject);
+        console.log(res);
+        return res;
     })
-    .then((res) => {
-        movieApp.getUserActorInfo(res);
+}
+
+movieApp.getPersonInfo = function(){
+    const personInfoPromise = movieApp.getData(movieApp.searchForActor, movieApp.getPersonInfoDataObject);
+    personInfoPromise.then((res) => {
+        // console.log('res', res);
+        movieApp.userSelectionObject.userActorSelectionID = res.results[0].id;
+        movieApp.userSelectionObject.profilePath = res.results[0].profile_path;
+        movieApp.userSelectionObject.name = res.results[0].name;
+    });
+    return personInfoPromise;
+}
+
+movieApp.getMovieInfo = function(){
+    const movieInfoPromise = movieApp.getData(movieApp.discoverMovie, movieApp.getMovieInfoDataObject);
+    movieInfoPromise.then((res) =>{
+        // console.log('movie res', res);
+        movieApp.userSelectionObject.movieTitle = res.results[0].title;
+        movieApp.userSelectionObject.movieOverView = res.results[0].overview;
+        movieApp.userSelectionObject.movieID = res.results[0].id;
+        movieApp.userSelectionObject.moviePoster = res.results[0].poster_path;
+        movieApp.userSelectionObject.moviePopularity = res.results[0].popularity;
+    });
+    return movieInfoPromise;
+}
+
+//use to get config data
+movieApp.getConfiguration = function(){
+    const configPromise = movieApp.getData(movieApp.searchConfig, movieApp.APIOnly);
+    configPromise.then((res) => {
+        movieApp.userSelectionObject.imageBaseURL = res.images.secure_base_url;
+        movieApp.userSelectionObject.posterSize = res.images.poster_sizes[1];
+        movieApp.userSelectionObject.stillSize = res.images.still_sizes[1];
+        movieApp.userSelectionObject.profileSize = res.images.profile_sizes[1];
         // console.log(res);
-    })
-    //THIS THEN STATEMENT SHOULD BE COVERED IN THE DOC READY AREA WITH THE WHEN STATEMENT
+    });
+    return configPromise
+    // console.log(res)
 }
 
-movieApp.getUserActorInfo = function(res){
-    // console.log(res);
-    //^^^^^RUN CONSOLE LOG TO GET MORE INFO ON ACTOR
-    movieApp.userSelectionObject.userActorSelectionID = res.results[0].id;
-    movieApp.userSelectionObject.profilePath = res.results[0].profile_path;
-    console.log(movieApp.userSelectionObject);
+
+movieApp.getVideos = function(){
+    movieApp.getData(movieApp.serchMovieTrailor, movieApp.APIOnly);
+    movieApp.getData(movieApp.serchMovieTrailor, `/movie/${movieApp.userSelectionObject.movieID}/videos`);
 }
 
-// movieApp.getData(movieApp.discoverMovie, null, movieApp.userActorSelectionID);
+
+// movieApp.getData(movieApp.discoverMovie, null, movieApp.userSelectionObject.userActorSelectionID);
 //^^^^^^ Technically this function pulls movies based on the actor that saved in user actor selection - wonky though
 
 
+//ask the api for an actor's data
+//push the id and poster end point to the actor object
+//use the id to ask the api for a list of movies
+//push the movie id, poster endpoint, video endpoint, movie rating, popularity, synopsis to the object
 
+
+//questions
+//is this readable
+//should I just create multiple functions that talk to the API
+//
